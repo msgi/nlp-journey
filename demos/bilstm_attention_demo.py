9a3ddaf2725c119643d31e914e1b93bdd2e2a7a8
msg901216 <1107306870@qq.com>
2019-05-05 19:19:01 +0800

    修复bug

--- demos/bilstm_attention_demo.py
+++ demos/bilstm_attention_demo.py
@@ -1,4 +1,5 @@
 from nlp.classfication.bilstm_attention import BiLSTMAttentionModel
+
 if __name__ == '__main__':
     model = BiLSTMAttentionModel(embedding_file='data/quora/GoogleNews-vectors-negative300.bin.gz')
     model.train()
