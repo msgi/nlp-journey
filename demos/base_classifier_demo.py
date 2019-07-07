01d37901246c6ac0b0f2fda127344774c0fc1b1d
msg901216 <1107306870@qq.com>
2019-07-08 07:36:40 +0800

    测试

--- demos/base_classifier_demo.py
+++ demos/base_classifier_demo.py
@@ -1,11 +1,11 @@
 import os
 import sys
-sys.path.append('e:/Workspace/pythons/nlp-journey')
+sys.path.append('/home/msg/workspace/pythons/nlp-journey')
 from nlp.classfication.basic.basic_classifier import TextClassifier
 if __name__ == '__main__':
-    base_classifier = TextClassifier(model_path='e:/Workspace/pythons/nlp-journey/demos/model/base/',
-                                     config_path='e:/Workspace/pythons/nlp-journey/demos/model/base/config.pkl', 
+    base_classifier = TextClassifier(model_path='./model/base/',
+                                     config_path='./model/base/config.pkl',
                                      train=True, 
-                                     vector_path='e:/Workspace/pythons/nlp-journey/demos/data/GoogleNews-vectors-negative300.bin.gz')
+                                     vector_path='./data/GoogleNews-vectors-negative300.bin.gz')
     out = base_classifier.predict('this is very good movie, i want to watch it again!')
     print(out)
