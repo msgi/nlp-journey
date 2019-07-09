ac80f8d3fb5838cbe4fd98ea6d6bf291da5cbb1a
msg901216 <1107306870@qq.com>
2019-07-09 10:12:58 +0800

    rcnn改造完成

--- nlp/classfication/dl/rcnn_classifier.py
+++ nlp/classfication/dl/rcnn_classifier.py
@@ -6,16 +6,6 @@ from .basic_classifier import TextClassifier
 
 
 class TextRCNNClassifier(TextClassifier):
-    """
-        RCNN 整体的模型构建流程如下：
-
-    　　1）利用Bi-LSTM获得上下文的信息，类似于语言模型。
-    　　2）将Bi-LSTM获得的隐层输出和词向量拼接[fwOutput, wordEmbedding, bwOutput]。
-    　　3）将拼接后的向量非线性映射到低维。
-    　　4）向量中的每一个位置的值都取所有时序上的最大值，得到最终的特征向量，该过程类似于max-pool。
-    　　5）softmax分类。
-    """
-
     def build_model(self):
         inputs = Input((self.maxlen,))
         embedding = Embedding(len(self.embeddings),
